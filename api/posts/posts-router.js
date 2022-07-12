0adb585cdb7f7ad36057afc49288e2905cbb365a
Muniba Ahmed <mahmed93@berkeley.edu>
2022-07-12 13:22:56 -0700

    reached mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -144,6 +144,24 @@ router.delete("/:id", async (req, res) => {
 });
 // 6	GET	/api/posts/:id/comments	Returns an array of all the comment objects associated with the post with the specified id
 
-router.get("/:id/comments", (req, res) => {});
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const posty = await Posts.findById(req.params.id);
+    if (!posty) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const getComments = await Posts.findPostComments(req.params.id);
+      res.status(200).json(getComments);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The comments information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
 
 module.exports = router;
